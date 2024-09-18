cf0b287f3af24b707cbc297b69f26c966b23355d
4nder123 <ander.jarv@gmail.com>
2024-09-18 15:32:41 +0300

    Added socket.io for live kodutoo updates

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const app = express()
 const axios = require('axios')
 const cors = require('cors')
 const path = require('path');
@@ -10,9 +9,15 @@ const fsPromises = require('fs').promises;
 const fs = require('fs')
 const url = require('url')
 const cookieParser = require("cookie-parser")
-const {verifyJWT, isloggedin} = require('./middleware/jwt_verify.js')
+const {verifyJWT} = require('./middleware/jwt_verify.js')
 const nodeCron = require("node-cron");
 const bodyParser = require("body-parser");
+const socketIo = require("socket.io");
+const http = require('http');
+
+const app = express()
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
@@ -21,9 +26,7 @@ app.set('view engine','ejs');
 
 app.engine('ejs', require('ejs').__express);
 
-app.use(cors({
-    orgin: '*'
-}))
+app.use(cors())
 
 app.use(bodyParser.json({ extended: false }))
 
@@ -34,6 +37,14 @@ app.use(express.urlencoded({extended: false}))
 const usersDB = () => {
     return { "users": JSON.parse(fs.readFileSync('./database/users.json'))}
 }
+
+io.engine.use(cookieParser());
+io.engine.use(verifyJWT);
+io.on('connection', (socket) => {
+    socket.on('liveUpdate', () => {
+      socket.join(socket.request.id);
+    });  
+});
 
 const job = nodeCron.schedule("0 0 0 * * *", () => {
     const userdb = usersDB();
@@ -52,48 +63,59 @@ const job = nodeCron.schedule("0 0 0 * * *", () => {
   });
 
 
-app.post("/savedone", verifyJWT, function(req, res){
-    const filePath = path.join(__dirname, `./database/user-saved-info/${res.id}.json`);
-    if (fs.existsSync(filePath)) {
-        let info = JSON.parse(fs.readFileSync(filePath));
+  app.post("/savedone", verifyJWT, function (req, res) {
+    const filePath = path.join(__dirname, `./database/user-saved-info/${req.id}.json`);
 
-        switch (req.body.action) {
-            case "ev-save":
-                info.done.push(req.body.info);
-                break;
-            case "ev-remove":
-                info.done = info.done.filter(done => done !== req.body.info);
-                break;
-            case "high-save":
-                let isSaved = info.highlight.find(high => high[0] === req.body.info[0]);
-                if(isSaved){
-                    isSaved[1] = req.body.info[1]
-                }
-                else {
-                    info.highlight.push(req.body.info);
-                }
-                break;
-            case "high-remove":
-                info.highlight = info.highlight.filter(high => high[0] !== req.body.info);
-                break;
-            default:
-                break;
+    const readFileData = () => {
+        if (fs.existsSync(filePath)) {
+            try {
+                return JSON.parse(fs.readFileSync(filePath));
+            } catch (error) {
+                return { done: [], highlight: [] };
+            }
         }
+        return { done: [], highlight: [] };
+    };
 
-        fs.writeFileSync(filePath, JSON.stringify(info));
-    } else {
-        let data;
-        if (req.body.action === "ev-save") {
-            data = { done: [req.body.info], highlight: [] };
-        } else if (req.body.action === "high-save") {
-            data = { done: [], highlight: [req.body.info] };
-        } else {
-            data = { done: [], highlight: [] };
+    const writeFileData = (data) => {
+        try {
+            fs.writeFileSync(filePath, JSON.stringify(data));
+        } catch (error) {
+            return res.status(500).send("Error saving data");
         }
-        fs.writeFileSync(filePath, JSON.stringify(data));
+    };
+
+    const info = readFileData();
+    const { action, info: reqInfo } = req.body;
+    switch (action) {
+        case "ev-save":
+            if (!info.done.includes(reqInfo)) {
+                if(info.highlight.some(high => high[0] === reqInfo)){
+                    info.highlight = info.highlight.filter(high => high[0] !== reqInfo);
+                }
+                info.done.push(reqInfo);
+            }
+            break;
+        case "ev-remove":
+            info.done = info.done.filter(done => done !== reqInfo);
+            break;
+        case "high-save":
+            info.highlight = info.highlight.filter(high => high[0] !== reqInfo[0]);
+            info.highlight.push(reqInfo);
+            break;
+        case "high-remove":
+            info.highlight = info.highlight.filter(high => high[0] !== reqInfo);
+            break;
+        default:
+            return res.status(400).send("Invalid action");
     }
-    return res.status(204).send()
-})
+
+    writeFileData(info);
+    io.to(req.id).emit('updateUserData', reqInfo, action);
+    return res.status(204).send();
+}, (err, req, res, next) => {
+    res.status(500)
+});
 
 app.post('/register', async (req, res) => {
     const usersdb = usersDB()
@@ -138,7 +160,7 @@ return !!urlPattern.test(urlString);
 
 app.post('/settingssave', verifyJWT,function(req, res){
     const usersdb = usersDB()
-    const user = usersdb.users.find(person => person.id === res.id)
+    const user = usersdb.users.find(person => person.id === req.id)
     if(!isValidUrl(req.body.ois)){
         req.body.ois = ""
     }
@@ -153,7 +175,9 @@ app.post('/settingssave', verifyJWT,function(req, res){
         return res.redirect("/")
     }
     res.redirect(new URL(req.headers.referer).pathname)
-})
+}, (err, req, res, next) => {
+    res.status(500);
+});
 
 app.post('/login', async(req, res) =>{
     const usersdb = usersDB()
@@ -164,11 +188,11 @@ app.post('/login', async(req, res) =>{
     else{
         if(await bcrypt.compare(req.body.password, user.password)){
             if(user.allow === 1){
-                const token = jwt.sign({"id": user.id}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20m' })
+                const token = jwt.sign({"id": user.id}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
                 const rtoken = jwt.sign({"id": user.id, "date": Date.now()}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' })
                 user.token.push(rtoken)
-                res.cookie('Token', token, { maxAge: 1200000, httpOnly: true })
-                res.cookie('RToken', rtoken, { maxAge: 2592000000, httpOnly: true })
+                res.cookie('Token', token, { maxAge: 600000, secure:true, httpOnly: true })
+                res.cookie('RToken', rtoken, { maxAge: 2592000000, secure:true, httpOnly: true })
                 await fsPromises.writeFile(
                     path.join(__dirname, '/database/users.json'),
                     JSON.stringify(usersdb.users, null, 4)
@@ -185,58 +209,71 @@ app.post('/login', async(req, res) =>{
     }
 })
 
-app.post('/getevents', verifyJWT, async(req,res) => {
-    let url = req.body.calurl
+app.get('/getevents', verifyJWT, async(req,res) => {
+    let url = req.query.calurl
     if(url!=""){
         url = new URL(url)
         axios.get(url.href).then(async function (response) {
-            if(url.hostname == "moodle.ut.ee"){
-                res.send(await isctoo(response.data.toString(), res.id))
+            if(url.hostname.includes("moodle")){
+                res.send(JSON.stringify(await isctoo(response.data.toString(), req.id)))
             }
             else if(url.hostname == "ois2.ut.ee"){
                 res.send(isctund(response.data.toString()))
             }
             else{
-                return ""
+                return res.send(500)
             }
         }).catch(function (error) {
-            res.send("")
+            res.send(500)
         })
     }
     else{
-        res.send("")
+        res.send(500)
     }
-})
+}, (err, req, res, next) => {
+    res.send(500)
+});
 
 app.get('/', verifyJWT, function(req,res){
-    const user = usersDB().users.find(person => person.id === res.id)
-    res.render('index.ejs', {id: res.id, moodle:user.moodle, ois:user.ois, pohivaade:user.pohivaade})
-})
+    const user = usersDB().users.find(person => person.id === req.id)
+    res.render('index.ejs', {id: req.id, moodle:user.moodle, ois:user.ois, pohivaade:user.pohivaade})
+}, (err, req, res, next) => {
+    res.redirect("login");
+});
 
-app.get('/register', isloggedin,function(req,res){
+app.get('/register', verifyJWT,function(req,res){
+    res.redirect("/");
+}, (err, req, res, next) => {
     res.render('register.ejs' , {message: ""});
-})
+});
 
-app.get('/login', isloggedin,function(req,res){
+app.get('/login', verifyJWT,function(req,res){
+    res.redirect("/");
+}, (err, req, res, next) => {
     if(Object.keys(req.query).length === 0){
         res.render('login.ejs', {message: "", email: "", type: ""});
-    }
-    else{
+    } else {
         res.render('login.ejs', {message: req.query.message, email: req.query.email, type: req.query.type});
     }
-})
+});
 
 app.get('/tunniplaan', verifyJWT, function(req,res){
     res.sendFile(path.join(__dirname + "/js-modules/tunniplaan.js"));
-})
+}, (err, req, res, next) => {
+    res.send(500)
+});
 
 app.get('/kodutoo', verifyJWT, function(req,res){
     res.sendFile(path.join(__dirname + "/js-modules/kodutoo.js"));
-})
+}, (err, req, res, next) => {
+    res.send(500)
+});
 
 app.get('/long-press-event', verifyJWT, function(req,res){
     res.sendFile(path.join(__dirname + "/js-modules/long-press-event.min.js"));
-})
+}, (err, req, res, next) => {
+    res.send(500)
+});
 
 app.get('/login.css', function(req, res) {
     res.sendFile(path.join(__dirname + "/views/login.css"));
@@ -266,4 +303,4 @@ app.get('/logout', function(req, res) {
     res.redirect("/")
   });
 
-app.listen(3000)
+server.listen(3000)
--- js-modules/kodutoo.js
+++ js-modules/kodutoo.js
@@ -1,26 +1,58 @@
 import { Calendar } from 'https://cdn.skypack.dev/@fullcalendar/core';
 import listPlugin from 'https://cdn.skypack.dev/@fullcalendar/list';
 import etLocale from 'https://cdn.skypack.dev/@fullcalendar/core/locales/et';
+import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
+
+
 
 const save = "ev-save"
 const rm = "ev-remove"
 const high_save = "high-save"
 const high_rm = "high-remove"
 let color = localStorage.getItem('high') || "rgb(255, 255, 0, 0.25)";
-const highlightButton = "<div class='high' style='float:right;' data-long-press-delay='500'><i id='highlight' class='fa-solid fa-highlighter fa-lg'></i></div>"
+const highlightButton = "<div id='highlight' style='display:table-cell;text-align:right;vertical-align:middle;height:100%;padding-left:10px;' data-long-press-delay='500'><i id='highlight' class='fa-solid fa-highlighter fa-lg'></i></div>"
+const socket = io.connect('http://localhost:3000',{
+    reconnection: true,
+    reconnectionAttempts: Infinity,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    forceNew: true,
+    secure: true 
+});
+socket.on("connect", () => {
+    socket.emit('liveUpdate');
+});
 
-function changeColor(){
-    console.log('change')
+function changeHighColor(new_color){
+    localStorage.setItem("high", new_color);
+    color = new_color;
+}
+
+function setExProps(event, status, color){
+    event.setExtendedProp("status", status);
+    event.setExtendedProp("color", color);
+    highlightEvent(event.id, color)
 }
 
 function savetodb(evdone, action){
-    fetch("/savedone", {method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"info":evdone, "action": action})
-    })
+    try {
+        fetch("/savedone", {method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"info":evdone, "action": action})
+        })
+    } catch (err){
+        console.log(err);
+    }
 }
+
+function highlightEvent(event_id, color){
+    const eventRow = document.querySelector("[id='"+event_id+"']").closest('tr');
+    eventRow.style.backgroundColor = color;
+}
+
+
 function loadKodutoo(moodle) {
     const calendarEl = document.getElementById('kodutoo');
     const calendar = new Calendar(calendarEl, {
@@ -46,50 +78,37 @@ function loadKodutoo(moodle) {
         locale: etLocale,
         defaultTimedEventDuration: 0,
         events: {
-            url: '/getevents',
-            method: 'POST',
-            extraParams: {
-                calurl: moodle
-            }
+            url: '/getevents?' + new URLSearchParams({calurl:moodle})
         },
         eventDidMount: function(info) {
-            let done = info.event.extendedProps.status === "done";
-            let isHighlighted = info.event.extendedProps.status === "high";
-            let highcolor = info.event.extendedProps.color
-            let longpress = false;
+            const event = info.event;  
+            const eventId = info.event.id;
             let tooltip = null;
-          
-            const eventTitleElement = info.el.querySelector(".fc-list-event-title");
-            const eventTitle = info.event.title;
+            let longpress = false;
             
             const handleClick = function (e) {
               const targetId = e.target.id;
+              const isDone = event.extendedProps.status === "done"
+              const isHigh = event.extendedProps.status === "high"
               if (info.el.contains(e.target)) {
-                if (!done && targetId !== 'highlight') {
-                  savetodb(eventTitle, save);
-                  savetodb(eventTitle, high_rm);
-                  isHighlighted = false;
-                  info.el.style.backgroundColor = "";
-                  eventTitleElement.innerHTML = `<a><s>${eventTitle}</s></a>`;
-                  done = true;
-                } else if (done && targetId !== 'highlight') {
-                  savetodb(eventTitle, rm);
-                  eventTitleElement.innerHTML = `<a>${eventTitle}</a>${highlightButton}`;
-                  done = false;
-                } else if (!isHighlighted && targetId === 'highlight' && !longpress) {
-                  savetodb([eventTitle, color], high_save);
-                  isHighlighted = true;
-                  info.el.style.backgroundColor = color;
-                } else if (isHighlighted && targetId === 'highlight' && !longpress) {
-                  savetodb(eventTitle, high_rm);
-                  isHighlighted = false;
-                  info.el.style.backgroundColor = "";
+                if (!isDone && targetId !== 'highlight') {
+                  savetodb(eventId, save);
+                  setExProps(event, "done", "") 
+                } else if (isDone && targetId !== 'highlight') {
+                  savetodb(eventId, rm);
+                  setExProps(event, "", "") 
+                } else if (!isHigh && targetId === 'highlight' && !longpress) {
+                  savetodb([eventId, color], high_save);
+                  setExProps(event, "high", color)
+                } else if (isHigh && targetId === 'highlight' && !longpress) {
+                  savetodb(eventId, high_rm);
+                  setExProps(event, "", "") 
                 }
               }
             };
 
             const handleLongClick = function (e) {
-                if(e.target.id === 'highlight'){
+                if(e.target.id === 'highlight' && tooltip === null){
                     longpress = true;
                     tooltip = new bootstrap.Tooltip(e.target, {
                         title: "<div class='box red'></div><div class='box orange'></div><div class='box yellow'></div>",
@@ -99,60 +118,55 @@ function loadKodutoo(moodle) {
                         container: 'body'
                     });
                     tooltip.show()
-                    document.querySelector('.red').addEventListener("click", function (e){
-                        localStorage.setItem('high','rgb(255, 0, 0, 0.25)');
-                        color = 'rgb(255, 0, 0, 0.25)';
-                        savetodb([eventTitle, color], high_save);
-                        info.el.style.backgroundColor = color;
+                    document.querySelector('.red').addEventListener("mousedown", function (e){
+                        changeHighColor('rgb(255, 0, 0, 0.25)')
+                        savetodb([eventId, color], high_save);
+                        setExProps(event, "high", color) ;
                     })
-                    document.querySelector('.orange').addEventListener("click", function (e){
-                        localStorage.setItem('high','rgb(255, 165, 0, 0.3)');
-                        color = 'rgb(255, 165, 0, 0.25)';
-                        savetodb([eventTitle, color], high_save);
-                        info.el.style.backgroundColor = color;
+                    document.querySelector('.orange').addEventListener("mousedown", function (e){
+                        changeHighColor('rgb(255, 165, 0, 0.3)');
+                        savetodb([eventId, color], high_save);
+                        setExProps(event, "high", color);
                     })
-                    document.querySelector('.yellow').addEventListener("click", function (e){
-                        localStorage.setItem('high','rgb(255, 255, 0, 0.25)');
-                        color = 'rgb(255, 255, 0, 0.25)';
-                        savetodb([eventTitle, color], high_save);
-                        info.el.style.backgroundColor = color;
+                    document.querySelector('.yellow').addEventListener("mousedown", function (e){
+                        changeHighColor('rgb(255, 255, 0, 0.25)');
+                        savetodb([eventId, color], high_save);
+                        setExProps(event, "high", color);
                     })
+                    document.addEventListener("mousedown", function () {
+                        if(tooltip !== null){
+                            tooltip.dispose()
+                            tooltip = null
+                        }
+                        longpress = false;
+                    });
                 }
             }
-
-            const disposeTooltip = function (e) {
-                if(!longpress && tooltip !== null){
-                    tooltip.dispose()
-                    tooltip = null
-                }
-                longpress = false;
-            }
-
-            if (done) {
+            highlightEvent(eventId, event.extendedProps.color);
+            info.el.addEventListener("click", handleClick);
+            info.el.addEventListener('long-press', handleLongClick);
+        },
+        eventContent: function(info) {
+            let eventTitleElement = document.createElement('div');
+            const eventTitle = info.event.title;
+            eventTitleElement.setAttribute("id", info.event.id);
+            eventTitleElement.style.display =  "table";
+            eventTitleElement.style.width = "100%";
+            if (info.event.extendedProps.status === "done") {
                 eventTitleElement.innerHTML = `<a><s>${eventTitle}</s></a>`;
-            } else if (isHighlighted) {
-                info.el.style.backgroundColor = highcolor;
-                eventTitleElement.innerHTML = `<a>${eventTitle}</a>${highlightButton}`;
             } else {
                 eventTitleElement.innerHTML = `<a>${eventTitle}</a>${highlightButton}`;
             }
-          
-            info.el.addEventListener("click", handleClick);
-            info.el.addEventListener('long-press', handleLongClick);
-            document.addEventListener("click", disposeTooltip)
-          
-            return function() {
-              info.el.removeEventListener("click", handleClick);
-              info.el.removeEventListener('long-press', handleLongClick);
-              document.removeEventListener("click", disposeTooltip)
-            };
-          },
+            let arrayOfDomNodes = [ eventTitleElement ];
+            return { domNodes: arrayOfDomNodes };
+        },
         loading: function(isLoading){
             if(!isLoading){
                 document.querySelector(".loader").classList.add("loader--hidden")
             }
         },
         lazyFetching: true,
+        progressiveEventRendering: true,
         contentHeight: "auto",
         initialView: 'listYear',
         headerToolbar: {
@@ -165,7 +179,20 @@ function loadKodutoo(moodle) {
     document.getElementById('kodutoo').querySelector(".fc-header-toolbar").classList.add("kodutooheader");
     document.getElementById('kodutoo').getElementsByClassName("fc-view-harness")[0].style = "top: 55px;" ;
     document.getElementById('kodutoo').getElementsByClassName("fc-settings-button")[0].innerHTML = '<i class="fa fa-gear"></i>';
-
+    socket.on('updateUserData', (id, action) => {
+        const eventId = calendar.getEventById(typeof id === 'string'? id : id[0]);
+        if(eventId){
+            if (action === save) {
+                setExProps(eventId, "done", "")
+            } else if (action === high_save){
+                setExProps(eventId, "high", id[1])          
+            } else {
+                setExProps(eventId, "", "")
+            }
+        } else {
+            calendar.refetchEvents();
+        }
+    });
     return calendar
 }
 
