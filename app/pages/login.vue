8303f18d79a3edab06a200a2a484b6a525950f59
anderjar <ander.jarv@ut.ee>
2025-09-29 23:33:45 +0300

    add authentication pages with styles for login and registration

---
+++ app/pages/login.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import "~/assets/css/auth.css";
+
+definePageMeta({
+  layout: "auth",
+});
+</script>
+
+<template>
+  <div class="auth-page">
+    <div class="form">
+      <div v-if="false" class="error"></div>
+      <div v-if="false" class="success">{{ $t("auth.accountCreated") }}</div>
+      <form @submit.prevent="">
+        <input
+          name="email"
+          type="email"
+          id="email"
+          placeholder="email"
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          id="password"
+          placeholder="parool"
+          required
+        />
+        <button type="submit">{{ $t("auth.login") }}</button>
+        <p class="message">
+          {{ $t("auth.noAccount") }}
+          <NuxtLink to="/register"> {{ $t("auth.createAccount") }} </NuxtLink>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
