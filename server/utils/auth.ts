3ecfea57e43ad0476914df7db21b21fbe0a38364
anderjar <ander.jarv@ut.ee>
2025-10-16 00:55:26 +0300

    add: configure PostgreSQL database for better-auth integration

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,13 +1,27 @@
 import { betterAuth } from "better-auth";
 import { emailOTP } from "better-auth/plugins";
-import Database from "better-sqlite3";
+import { Pool } from "pg";
 
 export const auth = betterAuth({
-  database: new Database(".data/auth.sqlite"),
+  database: new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
+  }),
+  trustedOrigins: ["http://localhost:3000", "http://192.168.1.5:3000"],
+  session: {
+    maxAge: 60 * 60 * 24 * 30,
+    updateAge: 60 * 60 * 24,
+  },
   advanced: {
     cookies: {
       session_token: {
         name: process.env.NODE_ENV === "production" ? "__Host-SID" : "SID",
+        attributes: {
+          sameSite: "strict",
+        },
       },
     },
   },
